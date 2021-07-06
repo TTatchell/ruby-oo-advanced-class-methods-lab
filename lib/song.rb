7bd2fa250e476a302a73994c2fcf492fab3fc689
TTatchell <tim.tatchell24@gmail.com>
2021-07-06 22:00:59 +1000

    upload

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,57 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song
   @@all = []
+
+  def self.create
+     song = self.new
+     @@all << song
+     song
+  end
+
+  def self.new_by_name(name)
+      song = self.new
+      song.name = name
+      song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort {|a,b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(string)
+    song = self.new
+    song.artist_name, tmp = string.split(' - ')
+    song.name = tmp.chomp('.mp3')
+    #song.artist_name, song.name..chomp('.mp3') = string.split(' - ')
+    song
+  end
+
+  def self.create_from_filename(string)
+    song = self.new
+    song.artist_name, tmp = string.split(' - ')
+    song.name = tmp.chomp('.mp3')
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 
   def self.all
     @@all
@@ -9,5 +60,4 @@ class Song
   def save
     self.class.all << self
   end
-
 end
